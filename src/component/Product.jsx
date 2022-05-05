34de1b18afabec0e6dfc1d5ef46c11a845cf073c
Alexis Duque <alexis.duque@bewe.io>
2022-05-05 08:10:32 -0500

    fix

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -16,16 +16,16 @@ const Product = () => {
     const addProduct = (product) => {
         dispatch(addCart(product));
     }
-    const getProduct = async () => {
-        setLoading(true);
-        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-        setProduct(await response.json());
-        setLoading(false);
-    }
-    useEffect(() => {
+    // const getProduct = async () => {
+    //     setLoading(true);
+    //     const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    //     setProduct(await response.json());
+    //     setLoading(false);
+    // }
+    // useEffect(() => {
        
-        getProduct();
-    }, []);
+    //     getProduct();
+    // }, []);
 
     const Loading = () => {
         return(
